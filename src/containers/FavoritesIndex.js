a73ebb6faea429498772847b9a9384f9557c8481
katsuomi <mugichak1227@gmail.com>
2019-12-14 18:41:21 +0900

    お気に入り機能のUI調整

--- src/containers/FavoritesIndex.js
+++ src/containers/FavoritesIndex.js
@@ -7,15 +7,66 @@ import { useSelector } from 'react-redux';
 const FavoritesIndex = () => {
   const allFavorites = useSelector(state => state.favorites.allFavorites);
   console.log(allFavorites)
+  const settings = {
+    className: 'center',
+    centerMode: true,
+    centerPadding: '300px',
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    swipeToSlide: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    dots: true,
+    dotsClass: 'slick-dots',
+    responsive: [
+      {
+        breakpoint: 1025,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          centerPadding: '200px',
+        }
+      },
+      {
+        breakpoint: 900,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          centerPadding: '150px',
+        }
+      },
+      {
+        breakpoint: 850,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          centerPadding: '100px',
+        }
+      },
+      {
+        breakpoint: 700,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          centerPadding: '100px',
+        }
+      }
+    ]
+  };
+
   return(
     <>
-      {allFavorites.map((page,i) => (
-        <div key={i}>
-          <Link to={`/books/${page.id}?index=${page.index}`}>
-            <img src={page.imageUrl} alt="favorite" width='50%' />
-          </Link>
-        </div>
-      ))}
+      <p className="favoritePagesFontStyle">いいねしたページ({allFavorites.length})</p>
+      <Slider {...settings}>
+        {allFavorites.map((page,i) => (
+          <div key={i}>
+            <Link to={`/books/${page.id}?index=${page.index}`}>
+              <img src={page.imageUrl} alt="favorite" className='top-slider-image-style' />
+            </Link>
+          </div>
+        ))}
+      </Slider>
     </>
   );
 }
