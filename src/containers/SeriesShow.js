18546897f7fa67647a7914924e495e574b523788
katsuomi <mugichak1227@gmail.com>
2019-12-15 13:07:34 +0900

    LazyLoader導入

--- src/containers/SeriesShow.js
+++ src/containers/SeriesShow.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
+import LazyLoad from 'react-lazyload';
 import { Link } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
 import { useActions } from '../actions';
@@ -22,9 +23,9 @@ const SeriesShow = () => {
         {books && books.map((book,i) => (
           <Grid item xs={6} md={6} lg={4} key={i}>
             <Link to={"/books/"+book.id}>
-              <Card>
+              <LazyLoad height={500} >
                 <img src={book.image} alt={book.title} className='allBooksImg' />
-              </Card>
+              </LazyLoad>
             </Link>
           </Grid>
         ))}
